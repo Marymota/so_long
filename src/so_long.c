138f55845103fcf4af7906892787c2034985e4e3
marymota <dg.marciamota@gmail.com>
2021-10-11 23:54:52 +0100

    still trying to figure out how to overlap images...

--- src/so_long.c
+++ src/so_long.c
@@ -9,19 +9,25 @@
 */
 #include "so_long.h"
 
+void set_player(t_game *game, int y, int x)
+{
+	game->player.horizontal = x;
+	game->player.vertical = y;
+	mlx_put_image_to_window(game->mlx, game->mlx_win, game->character, x * 100, y * 100);
+}
+
 void init_img(t_game *game)
 {
-	game->relative_path_wall = "./newassets/wall.xpm";
-	game->wall = mlx_xpm_file_to_image(game->mlx, game->relative_path_wall, &game->img_width, &game->img_height);
-	game->relative_path_path = "./assets/path.xpm";
-	game->img = mlx_xpm_file_to_image(game->mlx, game->relative_path_path, &game->img_width, &game->img_height);
+	game->relative_path_path = "./newassets/wall.xpm";
+	game->path = mlx_xpm_file_to_image(game->mlx, game->relative_path_path, &game->i, &game->j);
+	game->relative_path_wall = "./assets/path.xpm";
+	game->wall = mlx_xpm_file_to_image(game->mlx, game->relative_path_wall, &game->i, &game->j);
 	game->relative_path_character = "./newassets/character_small_1.xpm";
-	game->character = mlx_xpm_file_to_image(game->mlx, game->relative_path_character, &game->img_width, &game->img_height);
+	game->character = mlx_xpm_file_to_image(game->mlx, game->relative_path_character, &game->i, &game->j);
 	game->relative_path_collectible = "./newassets/seedsr.xpm";
-	game->collectible = mlx_xpm_file_to_image(game->mlx, game->relative_path_collectible, &game->img_width, &game->img_height);
+	game->collectible = mlx_xpm_file_to_image(game->mlx, game->relative_path_collectible, &game->i, &game->j);
 	game->relative_path_exit ="./assets/Exit.xpm";
-	game->exit = mlx_xpm_file_to_image(game->mlx, game->relative_path_exit, &game->img_width, &game->img_height);
-
+	game->exit = mlx_xpm_file_to_image(game->mlx, game->relative_path_exit, &game->i, &game->j);
 }
 
 void draw_board(t_game *game)
@@ -35,21 +41,13 @@ void draw_board(t_game *game)
 		while (x < game->board_width)
 		{
 			if (game->board_str[i] == '1')
-			{
 				mlx_put_image_to_window(game->mlx, game->mlx_win, game->wall, x * 100, y * 100);
-			}
 			else if (game->board_str[i] == 'E')
-			{
 				mlx_put_image_to_window(game->mlx, game->mlx_win, game->exit, x * 100, y * 100);
-			}
 			else if (game->board_str[i] == 'P')
-			{
-				mlx_put_image_to_window(game->mlx, game->mlx_win, game->character, x * 100, y * 100);
-			}
+				set_player(game, y, x);
 			else if (game->board_str[i] == 'C')
-			{
 				mlx_put_image_to_window(game->mlx, game->mlx_win, game->collectible, x * 100, y * 100);
-			}
 			++i;
 			++x;
 		}
@@ -68,10 +66,7 @@ void draw_path(t_game *game)
 	{
 		while (x < game->board_width)
 		{
-			if (game->board_str[i])
-			{
-				mlx_put_image_to_window(game->mlx, game->mlx_win, game->img, x * 100, y * 100);
-			}
+			mlx_put_image_to_window(game->mlx, game->mlx_win, game->path, x * 100, y * 100);
 			++i;
 			++x;
 		}
@@ -100,7 +95,6 @@ void read_board(t_game *game, char *board)
 	game->board_str = ft_strjoin(game->board_str, line);
 	++game->board_height;
 	game->board_width = ft_strlen(line);
-	printf("\n%s\n", game->board_str);
 	free(line);
 	close(fd);
 }
@@ -114,11 +108,12 @@ int main (int argc, char *argv[])
 	ft_memset(&game, 0, sizeof(t_game));
 	read_board(&game, argv[1]);
 	game.mlx = mlx_init();
-	game.mlx_win = mlx_new_window(game.mlx, game.board_width * 100, game.board_height * 100, "soLong");
+	game.mlx_win = mlx_new_window(game.mlx, (game.board_width * 100), (game.board_height * 100), "soLong");
 	init_img(&game);
 	draw_path(&game);
-	//draw_board(&game);
+	draw_board(&game);
 	mlx_loop(game.mlx);
+	return (0);
 }
 
 
--- src/so_long.h
+++ src/so_long.h
@@ -6,32 +6,49 @@
 #include <mlx.h>
 #include "../libs/libft/src/libft.h"
 
+typedef struct s_player
+{
+	int	horizontal;
+	int	vertical;
+}	t_player;
+
 typedef struct	s_game 
 {
+	t_player player;
+
 	void 	*mlx;
 	void 	*mlx_win;
+	
 	char	**board;
 	int		board_width;
 	int		board_height;
 	char	*board_str;
+
 	void	*img;
+
 	void	*wall;
 	void	*character;
 	void	*collectible;
 	void	*path;
 	void	*exit;
+
+	char 	*relative_path_path;
 	char 	*relative_path_wall;
 	char 	*relative_path_character;
 	char 	*relative_path_collectible;
-	char 	*relative_path_path;
 	char 	*relative_path_exit;
+
 	int 	img_width;
 	int		img_height;
+	int		i;
+	int		j;
+
 	char	*addr;
 	char 	*relative_path;
 	int		bits_per_pixel;
 	int		line_length;
 	int		endian;
+
 }	t_game;
 
 
